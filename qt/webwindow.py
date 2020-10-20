05ba9774f4bf950552590500d8df3a529394efd4
Jhin Yao <yaojing@ufotosoft.com>
2020-10-20 15:39:55 +0800

    close and restart locust

--- qt/webwindow.py
+++ qt/webwindow.py
@@ -13,16 +13,19 @@ class WebWindow(QWidget, Ui_WebWindow):
     def __init__(self):
         super(WebWindow, self).__init__()
         self.setupUi(self)
-        self.thread = Thread(target=self.start, args=())
+        self.thread = Thread(target=self.startLocust, args=())
         self.thread.start()
         time.sleep(1)
         self.browser.load(QUrl("http://localhost:8089"))
         
-    def start(self):
-        print(subprocess.call(["locust", "-f","./common/stress.py"]))
+    def startLocust(self):
+        self.process = subprocess.Popen(["locust", "-f","./common/stress.py"])
 
-    def stop(self):
-        pass
+    def stopLocust(self):
+        self.process.kill()
+    
+    def closeEvent(self, event):
+        self.stopLocust()
         
     
     
